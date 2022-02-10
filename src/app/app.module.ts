e0df81fa4a79da80cb04b16aff247fa3096779db
Andrii <dzura1996das@gmail.com>
2022-02-10 10:45:24 +0200

    added app.modules to git

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { LogoComponent } from './header/logo/logo.component';
+import { NavLinkComponent } from './header/nav-link/nav-link.component';
+import { NavLinksListComponent } from './header/nav-links-list/nav-links-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    LogoComponent,
+    NavLinkComponent,
+    NavLinksListComponent
   ],
   imports: [
     BrowserModule,
